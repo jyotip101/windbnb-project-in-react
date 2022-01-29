2d65acb85d1dcb30a6a1274f8a2c6207b3eb3d6c
jyoti patel <jyotipatelfyit77100@gmail.com>
2022-01-29 20:53:51 +0530

    submenu

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -8,25 +8,43 @@ const FormInput = () => {
         // onSubmit={submitHandler}
         className='grid   transition md:grid-cols-3  rounded-2xl grid-cols-1 border-0     sm:my-auto '
       >
-        <input
-          className='w-[90%] border-2 rounded-[14px] md:border-r-2 text-[#000] outline-none m-4 px-[1rem]  py-4'
-          type='text'
-          placeholder='Helsinki, Finland'
-          name='city'
-          // value={city}
-          // onChange={(e) => setCity(e.target.value)}
-        />
-        <input
-          className='w-[90%] border-2 rounded-[14px] md:border-r-2 text-[#000] outline-none m-4 px-[1rem]  py-4'
-          type='text'
-          placeholder='Add guests'
-          name='guest'
-          // value={guest}
-          // onChange={(e) => setGuest(e.target.value)}
-        />
+        <div className='w-[90%] md:border-2 md:rounded-[14px]  border-2 border-b-0 rounded-t-[14px]  text-[#000] outline-none md:m-4 px-[1rem]  py-2 sm:mb-0 sm:ml-4'>
+          <label
+            htmlFor='city'
+            className='text-[#000] text-[16px]  uppercase font-medium'
+          >
+            Location
+          </label>
+          <input
+            className='w-[100%] border-0 text-[#000] outline-none '
+            type='text'
+            placeholder='Helsinki, Finland'
+            name='city'
+            // value={city}
+            // onChange={(e) => setCity(e.target.value)}
+          />
+        </div>
+
+        <div className='w-[90%] md:border-2 md:rounded-[14px] border-2 text-[#000] outline-none md:m-4 sm:ml-4 sm:mb-4 px-[1rem] rounded-t-none rounded-b-[14px] py-2 sm:border-t-0'>
+          <label
+            htmlFor='guest'
+            className='text-[#000] text-[16px]  uppercase font-medium'
+          >
+            Guests
+          </label>
+          <input
+            className='w-[100%] border-0 text-[#000] outline-none '
+            type='text'
+            placeholder='Add guests'
+            name='guest'
+            // value={guest}
+            // onChange={(e) => setGuest(e.target.value)}
+          />
+        </div>
+
         <button
           className='flex justify-center items-center outline-none  py-[0.5rem] px-[1rem]  text-[#fff] bg-[#eb5757] 
-          border-[1px] rounded-[14px]  text-[18px] w-[10rem]  ml-4   md:my-4  
+          border-[1px] rounded-[14px]  text-[18px] w-[10rem]  ml-4 my-4  md:my-4  
         '
         >
           <i className='material-icons font-medium pr-1'>search</i>Search
--- src/components/Submenus.js
+++ src/components/Submenus.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import FormInput from './FormInput'
+import { useGlobalContext } from './Context'
 
 const Submenus = ({ items, setCity }) => {
-  // console.log(items)
+  const { isSubMenuOpen, openSubMenu, closeSubMenu } = useGlobalContext()
+
   return (
     <>
-      <div className='flex flex-col h-46 absolute top-0 left-0 w-full z-10 shadow bg-[#fff] p-4 transition-all '>
+      <div className='flex flex-col h-46 absolute top-0 left-0 w-full z-10 shadow  pt-8 bg-[#fff] p-4 transition-all '>
+        <p className='md:hidden visible  m-4 mt-[-1rem] ml-[0.5rem]  transition-all cursor-pointer z-20'>
+          Edit your search
+        </p>
+        <button
+          className=' md:hidden visible absolute top-[1rem] right-[2rem] transition-all cursor-pointer z-20'
+          onClick={closeSubMenu}
+        >
+          <i className='material-icons  font-bold '>close</i>
+        </button>
         <FormInput />
         <div className='flex sm:flex-row flex-col ml-4 '>
           <div className='w-[50%] transition'>
@@ -17,7 +28,7 @@ const Submenus = ({ items, setCity }) => {
                     // setGuest(item.split(', ')[1])
                   }}
                   key={index}
-                  className='text-[#4F4F4F] transition  my-8 w-max   flex    cursor-pointer hover:text-[#eb5757]'
+                  className='text-[#4F4F4F] transition  my-8 w-max   flex cursor-pointer hover:text-[#eb5757]'
                 >
                   <i className='material-icons  '>location_on</i>
                   {item},
